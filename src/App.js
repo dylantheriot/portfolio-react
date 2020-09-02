8c7ced9a8a7e41dc12252802937e50df9a127198
Dylan Theriot <dylantheriot@gmail.com>
2020-09-02 17:00:57 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './assets/index.css';
+
+function App() {
+  return (
+    <div className="p-8 h-screen bg-blue-600">
+    <h1 className="text-6xl font-avenir">
+      Hello Internet
+    </h1>
+    </div>
+  );
+}
+
+export default App;
