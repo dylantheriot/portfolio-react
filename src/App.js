532d72a52ceb89688e319e50ad42e61cfe31fcac
Dylan Theriot <dylantheriot@gmail.com>
2020-09-03 03:16:06 -0500

    added menu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './assets/index.css';
 import HomePage from './components/home_page/HomePage';
+import Menu from './components/menu/Menu';
 
 function App() {
   return (
-    <div className="bg-pastelBlue-100 min-h-full lg:h-full">
-      <div className="h-full font-avenir">
+    <>
+    <div className="relative bg-pastelBlue-100 min-h-full w-full bg-height">
+      <Menu />
+      <div className="relative h-full font-avenir">
         <HomePage />
       </div>
     </div>
+    </>
   );
 }
 
---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Menu() {
+  return (
+    <>
+      <div className="menu-height modified-w-screen">
+        <div className="text-xl md:text-4xl font-bold px-4 md:px-12 pt-4 flex flex-row justify-between w-full">
+          <h1>
+            MENU
+          </h1>
+          <h1>
+            CONTACT
+          </h1>
+        </div>
+
+      </div>
+    </>
+  );
+
+}
+
+export default Menu;
