7e75521ce4e5c80c83b82e323e813059a9a122eb
Dylan Theriot <dylantheriot@gmail.com>
2022-02-01 01:22:48 -0600

    added 656 assignment

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import './assets/index.css';
 import HomePage from './components/home_page/HomePage';
 import AboutPage from './components/about_page/AboutPage';
+import Page656 from './components/656/656Page';
 import { HashRouter, Route, Switch, useLocation } from "react-router-dom";
 import { __RouterContext } from 'react-router';
 import PortfolioPage from './components/portfolio_page/PortfolioPage';
@@ -23,6 +24,7 @@ function App(props) {
           <Route exact path='/experience' component={QualificationsPage} />
           <Route exact path='/leadership' component={ServicePage} />
           <Route exact path='/resume' render={() => (window.location = (window.location.origin + Resume))} />
+          <Route exact path='/656' component={Page656} />
         </Switch>
       </HashRouter>
     </>
